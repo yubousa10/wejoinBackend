98a7f77b5a7114d139b36945df023d8e89e89346
yubousa10 <yubousa10@163.com>
2015-10-27 18:32:51 -0400

    add

--- wejoin/wejoin/wejoin/settings.py
+++ wejoin/wejoin/wejoin/settings.py
@@ -44,7 +44,7 @@ INSTALLED_APPS = (
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': ('rest_framework.permissions.IsAdminUser',),
-#    'DEFAULT_AUTHENTICATION_CLASSES': ('rest_framework.authentication.TokenAuthentication', ),
+    'DEFAULT_AUTHENTICATION_CLASSES': ('rest_framework.authentication.TokenAuthentication', ),
     'PAGE_SIZE': 10
 }
 
